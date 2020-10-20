bd34712a7bbc530178bf626d874d32f4b5fce6f9
endar-firmansyah <mailuntukbelajar@gmail.com>
2020-10-20 21:22:49 +0700

    Learn list adn variable on Python

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,46 @@
 print("Hello World and Dicoding")
-print(" Hello everyone")
+print("Hello everyone")
+print("First time learning Python code")
+
+
+x = [5,10,15,20,25,30,35,40]
+
+print(x[5])
+print(x[-1])
+print(x[3:5])
+print(x[:5])
+print(x[-3:])
+print(x[1:7:2])
+
+
+x = [1,2,3,4]
+x[3]=6
+x.append(5)
+print(x)
+
+hewan = ["Kucing", "Ikan", "Ayam", "Kuda"]
+
+del hewan[2]
+print(hewan)
+
+del hewan[2]
+print(hewan)
+
+a = "Endar Firmansyah"
+
+print(a[3])
+print(len(a))
+print(a[6:11])
+
+a = "Hallo Dunia"
+
+print (a)
+
+t = (5, "program", 1+3j)
+print(t[1])
+print(t[-2])
+print(len(t))
+
+
+print(list("Hello"))
+
---
+++ variable.py
@@ -0,0 +1,25 @@
+x = 1000
+print("Nilai dari x adalah:", x)
+
+print("Hi {}" .format("Bro"))
+
+nama = "Firmansyah"
+
+print("Halo, %s Lagi apa?" %nama )
+
+
+nama = "Firmansyah"
+
+umur = 5
+
+print("Umur %s adalah %d tahun" % (nama, umur))
+
+
+nilai = [90, 85, 95, 100]
+
+print("Nilai ujian saya adalah: %s" % nilai)
+
+nilai = input("Masukkan nilai melalui keyboard:")
+print("Nilainya integer adalah:", (int(nilai)))
+print("Nilai Floatingnya adalah:",(float(nilai)))
+
