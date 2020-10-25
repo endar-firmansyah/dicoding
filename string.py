af56fe8def7820ec1f1b33b3f19774fb8e9bd271
endar-firmansyah <mailuntukbelajar@gmail.com>
2020-10-25 09:20:23 +0700

    Latihan

---
+++ string.py
@@ -0,0 +1,35 @@
+kata = "Endar Firmasnyah"
+kata = kata.upper()
+print(kata)
+
+
+kata1 = "Sedang Makan"
+kata1 = kata.lower()
+print(kata1)
+
+
+print ("Firmansyah     ".rstrip())
+
+print("     Endar".lstrip())
+
+print("       Endar Firmansyah     ".strip())
+
+
+kata2 = "codePYTHONcodecode"
+print(kata2.strip("code"))
+
+print("Endar Firmansyah" .startswith("E"))
+
+print("Endar Firmansyah".endswith("F"))
+
+print("62".join(["Danu","Wijaya","@"]))
+
+string = "Ayo belajar program bersama saya"
+
+print(string.replace("program", "Coding"))
+
+
+
+
+
+
